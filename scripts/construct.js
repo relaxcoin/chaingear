6d68e665ea183882b55358f5ec608b6cd9caa054
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-07-15 08:23:54 +0600

    renamed everything..

--- scripts/construct.js
+++ scripts/construct.js
@@ -16,6 +16,99 @@ function loadToml(filename, result) {
   }
 }
 
+function loadTomlCustom(filename, result) {
+  try {
+    var fullname = path.join(filename);
+    var data = fs.readFileSync(fullname);
+    var p = toml.parse(data);
+
+    p.system = p.name;
+    delete p.system;
+    if (p.metrics) {
+      if (p.metrics.rating) {
+        p.ratings = p.ratings || {};
+        p.ratings.rating = p.metrics.rating;
+        delete p.metrics.rating;
+      }
+
+      if (p.metrics.consensus) {
+        p.consensus = p.consensus || {};
+        p.consensus.consensus_name = p.metrics.consensus;
+        delete p.metrics.consensus;
+      }
+
+      if (p.metrics.algo) {
+        p.consensus = p.consensus || {};
+        p.consensus.hashing = p.metrics.algo;
+        delete p.metrics.algo;
+      }
+
+      if (p.metrics.status) {
+        p.descriptions = p.descriptions || {};
+        p.descriptions.state = p.metrics.status;
+        delete p.metrics.status;
+      }
+
+      if (p.metrics.type) {
+        p.descriptions = p.descriptions || {};
+        p.descriptions.tags = p.metrics.type;
+        delete p.metrics.type;
+      }
+
+      if (p.metrics.hashtag) {
+        p.descriptions = p.descriptions || {};
+        p.descriptions.hashtag = p.metrics.hashtag;
+        delete p.metrics.hashtag;
+      }
+
+      if (p.metrics.announce) {
+        p.events = p.events || {};
+        p.events.announcement = p.metrics.announce;
+        delete p.metrics.announce;
+      }
+
+      if (p.metrics.genesis) {
+        p.events = p.events || {};
+        p.events.genesis = p.metrics.genesis;
+        delete p.metrics.genesis;
+      }
+    }
+
+    if (p.links && p.links.length) {
+      for (var i = 0; i < p.links.length; i++) {
+        var link = p.links[i];
+        link.type = link.type || "";
+      }
+    }
+
+    if (p.dependencies) {
+      p.dependencies = p.dependencies.split(", ");
+    }
+
+    p.descriptions = p.descriptions || {};
+    p.descriptions.system_type = p.descriptions.system_type || "";
+
+    p.consensus = p.consensus || {};
+    p.consensus.consensus_type = p.consensus.consensus_type || "";
+
+    p.token = p.token || {};
+    p.token.token_name = p.token.token_name || "";
+
+    if (p.symbol) {
+      p.token = p.token || {};
+      p.token.token_symbol = p.symbol;
+      delete p.symbol;
+    }
+
+    result.push(p);
+  } catch (e) {
+    console.error("Parsing error on line " + e.line + ", column " + e.column +
+      ": " + e.message);
+    console.error(";at file " + fullname);
+    throw(e);
+  }
+}
+
 var walk = function (dir) {
   var results = [];
 
@@ -42,7 +135,7 @@ function act() {
   var filenames_toml = walk( path.join(__dirname, "..", "sources.toml") );
 
   for (var idx = 0; idx < filenames_toml.length; idx++) {
-    loadToml(filenames_toml[idx], result);
+    loadTomlCustom(filenames_toml[idx], result);
   }
 
   fs.writeFileSync(path.join(__dirname, "..", "chaingear.json"), JSON.stringify(result, null, 4));
